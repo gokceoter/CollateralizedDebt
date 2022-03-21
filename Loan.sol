3598e41a30d4ef79f342c4cf4d0515916628d0b9
Gökçe Öter <32647005+gokceoter@users.noreply.github.com>
2022-03-21 14:25:17 +0300

    Update Loan.sol

--- Loan.sol
+++ Loan.sol
@@ -1,50 +1,51 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8;
 
-import "./ierc20token.sol";
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IERC20.sol";
 
 contract Loan {
     address public lender;
     address public borrower;
-    IERC20Token public token;
+    IERC20 public token;
     uint256 public collateralAmount;
     uint256 public payoffAmount;
     uint256 public dueDate;
 
-    function Loan(
-        address _lender,
-        address _borrower,
-        IERC20Token _token,
-        uint256 _collateralAmount,
-        uint256 _payoffAmount,
-        uint256 loanDuration
+    constructor (
+        address  _lender,
+        address  _borrower,
+        IERC20  _token,
+        uint256  _collateralAmount,
+        uint256  _payoffAmount,
+        uint256  loanDuration
     )
-        public
+        public 
     {
         lender = _lender;
         borrower = _borrower;
         token = _token;
         collateralAmount = _collateralAmount;
         payoffAmount = _payoffAmount;
-        dueDate = now + loanDuration;
+        dueDate = block.timestamp + loanDuration;
     }
 
     event LoanPaid();
 
     function payLoan() public payable {
-        require(now <= dueDate);
+        require(block.timestamp <= dueDate);
         require(msg.value == payoffAmount);
 
         require(token.transfer(borrower, collateralAmount));
         emit LoanPaid();
-        selfdestruct(lender);
+        selfdestruct(payable(lender));
     }
 
     function repossess() public {
-        require(now > dueDate);
+        require(block.timestamp > dueDate);
 
         require(token.transfer(lender, collateralAmount));
-        selfdestruct(lender);
+        selfdestruct(payable(lender));
     }
 }
